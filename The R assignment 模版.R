fa756bcb0cb42a2a1517aa0c0b1b21b740452ffc
Nobady912 <mamatietie@gmail.com>
2024-01-25 13:29:34 -0500

    g
    nothing

---
+++ The R assignment 模版.R
@@ -0,0 +1,20 @@
+#################################################################################################################
+#Tie Ma
+#student number: 101316917
+#ECON 5027
+#################################################################################################################
+#Clean the environment
+rm(list = ls())
+dev.off()
+
+#set the direction
+
+
+#Lode the all package that necessary.
+#some of the package may not been used in this homework
+library("mass")
+library("ggplot2")
+
+#doctor who blue 
+col1 = "#003b6f"
+#################################################################################################################
---
+++ The acutally the first TA  in the R.R
@@ -0,0 +1,55 @@
+## clean the environment
+  rm(list = is())
+## lunch the enviroment
+  library("mass")
+
+  
+## Whateer you have the assisgment you need to be the ttimes
+# its wrose know to know exci to know that is hte fuckting 
+  
+#the
+  
+  11 == 
+    
+#using the <- is better 
+#when you discribe the arguemntin the lete, tis good in to discrion ite  data 
+    
+    
+4 ** 4
+  
+4^4
+
+cat("weeee", 123^1)
+cat()
+
+A <- 10
+A <- 12
+#The R will rename the variable.
+# for 
+
+
+print("hello word")
+A <- 123
+B <- 1234
+
+cat("fuck you", A * B)
+
+
+
+# the cinner produvt ofthe v1 and the v2 can aslo use the outer (v1, v2)
+
+install.packages("massin=")
+
+intall
+
+covariance <- matrix(c(1, 1.5, 
+                       1.5, 3), 
+                     norw = 2, ncol = 2, byrow = TRUE)
+
+
+
+
+
+
+
+
